60d37195aed6004e290b4cad5b2c75b6a76a3d2f
Glenn Gartner <glenngartner@gmail.com>
2016-05-23 00:28:50 -0400

    Remove lines from comment & post_spec.rb to pass test without warning

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   let(:post){Post.create!(title: "New Post Title", body: "New Post Body")}
   describe "attributes" do
     it "has title and body attributes" do
