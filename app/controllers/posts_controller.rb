06bcddf0723c7b0f6c4ea4330f18efc96cb66a16
Glenn Gartner <glenngartner@gmail.com>
2016-05-25 01:36:11 -0400

    Add SPAM filter. Complete assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    # Write over the first, and every fifth post with "Spam"
+    @posts.each_with_index do |post, index|
+      index += 1
+      if index == 1 || index % 5 == 0
+        post.update(title: "SPAM")
+      end
+    end
   end
 
   def show
