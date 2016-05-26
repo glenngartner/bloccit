a491fd434a47aa4a6da6d358c44b4bd7e755c039
Glenn Gartner <glenngartner@gmail.com>
2016-05-25 21:59:54 -0400

    Add logic to model, remove from controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    # Write over the first, and every fifth post with "Spam"
-    @posts.each_with_index do |post, index|
-      index += 1
-      if index == 1 || index % 5 == 0
-        post.update(title: "SPAM")
-      end
-    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  after_create :spamify
+
+  def spamify
+    if self.id == 1 || self.id % 5 == 0
+      update(title: "I've been SPAMified!")
+    end
+  end
 end
