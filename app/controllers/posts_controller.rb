95ad93f24bb22fdcc962c99629a74bf9f076ef9c
Glenn Gartner <glenngartner@gmail.com>
2016-06-06 23:37:59 -0400

    Complete checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,25 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
   def create
-# #9
    @post = Post.new
    @post.title = params[:post][:title]
    @post.body = params[:post][:body]
+   @topic = Topic.find(params[:topic_id])
+   @post.topic = @topic
 
-# #10
    if @post.save
-# #11
      flash[:notice] = "Post was saved successfully."
-     redirect_to @post
+     redirect_to [@topic, @post]
    else
-# #12
      flash.now[:alert] = "There was an error saving the post. Please try again."
      render :new
    end
@@ -40,7 +36,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.new[:alert] = "There was an error saving the post. Please try again."
       render :edit
@@ -52,7 +48,7 @@ class PostsController < ApplicationController
 
       if @post.destroy
         flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-        redirect_to posts_path
+        redirect_to @post.topic
       else
         flash.now[:alert] = "There was an error deleting the post."
         render :show
