25bbb028e6ec3fa64593310505fc4c4d4b1ce9bf
Glenn Gartner <glenngartner@gmail.com>
2016-06-09 00:35:32 -0400

    Complete assignment 36

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase if email.present? }
 
-    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+    before_save { self.name = name.downcase.split.map(&:capitalize).join(' ') if name.present?}
 
+    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+    
     validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
     validates :password, length: { minimum: 6 }, allow_blank: true
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { User.create!(name: "bloCCit uSer", email: "user@Bloccit.com", password: "password") }
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
